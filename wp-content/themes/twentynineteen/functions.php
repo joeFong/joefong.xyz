5c3d11c5bfb3a7b72a9ccfad4e7bb58a386bb43b
joe fong <joefong@joes-Air.hitronhub.home>
2019-10-24 22:28:33 -0700

    update

--- wp-content/themes/twentynineteen/functions.php
+++ wp-content/themes/twentynineteen/functions.php
@@ -1,5 +1,4 @@
 <?php 
-echo 'die'; die;
 	function my_scripts() {
 		wp_enqueue_style( 'joefong_style', get_template_directory_uri() . '/css/main.css');
 		wp_enqueue_script('joefong_js', get_template_directory_uri() . '/js/main.js' );
