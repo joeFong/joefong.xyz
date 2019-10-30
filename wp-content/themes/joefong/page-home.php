384a0a6c8373f04983c987cdef75138f237f94ef
joe fong <joefong@joes-Air.hitronhub.home>
2019-10-30 01:58:27 -0700

    update

--- wp-content/themes/joefong/page-home.php
+++ wp-content/themes/joefong/page-home.php
@@ -148,7 +148,15 @@ get_header();
     </script>
 
     <div id="github">
-        <p>Need a resume? Takes too long to create one? Make one in 2 minutes with resume-jf, a lighting fast litElement Web Component. Embeddable in any browser, and can be converted into a PDF in a click of a button.</p>
+        <p>Need a resume? But it takes too long to create one? Make one in 2 minutes with resume-jf, a lighting fast LitElement Web Component. Embeddable in any browser, and can be converted into a PDF in a click of a button.</p>
+        <p>
+            <a class="code-links" href="https://github.com/joeFong/resume-jf"><i class="fab fa-github"></i></a>
+            <a class="code-links" href="https://www.npmjs.com/package/resume-jf"><i class="fab fa-npm"></i></a>
+        </p>
+    </div>
+
+    <div id="github-mobile">
+        <p>Need a resume? But it takes too long to create one? Make one in 2 minutes with resume-jf, a lighting fast LitElement Web Component. Embeddable in any browser, and can be converted into a PDF in a click of a button.</p>
         <p>
             <a class="code-links" href="https://github.com/joeFong/resume-jf"><i class="fab fa-github"></i></a>
             <a class="code-links" href="https://www.npmjs.com/package/resume-jf"><i class="fab fa-npm"></i></a>
