9077b0f25cb9baf2ad41a376519ea14eaac036fb
joe fong <joefong@joes-Air.hitronhub.home>
2019-10-30 03:41:38 -0700

    update

--- wp-content/themes/joefong/page-home.php
+++ wp-content/themes/joefong/page-home.php
@@ -291,7 +291,7 @@ get_header();
 }
         </div>
         <p>Need a resume? But it takes too long to create one? Make one in 2 minutes with resume-jf, a lighting fast LitElement Web Component. Embeddable in any browser, and can be converted into a PDF in a click of a button.</p>
-        <p>
+        <p class="code-link-container">
             <a class="code-links" target="_blank" href="https://github.com/joeFong/resume-jf"><i class="fab fa-github"></i></a>
             <a class="code-links" target="_blank" href="https://www.npmjs.com/package/resume-jf"><i class="fab fa-npm"></i></a>
         </p>
