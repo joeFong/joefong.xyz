f8f27d37366581294ef4281c0cfe4a15305c00d5
joe fong <joefong@joes-Air.hitronhub.home>
2019-11-01 18:17:06 -0700

    update

--- wp-content/themes/joefong/js/main.js
+++ wp-content/themes/joefong/js/main.js
@@ -1,18 +1,16 @@
 window.onload = function() {
-    //Get the button:
-    var saveResumeEl = document.getElementById("saveResume");
-
-    // When the user scrolls down 20px from the top of the document, show the button
-    // window.onscroll = function() {scrollFunction()};
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            saveResumeEl.style.display = "block";
-
-            saveResumeEl.addEventListener("click", saveResume);
-
-        }
-    }
+    
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/json");
+    editor.setValue(resumeObj);
+    editor.removeSelectionMarker();
+    
+    editor.session.on('change', function(delta) {
+        var value = editor.getValue(); // or session.getValue
+        var resume = document.getElementById('resume');
+        resume.setAttribute('resume', JSON.stringify(JSON.parse(value)));
+    });
 
     // When the user clicks on the button, scroll to the top of the document
     function saveResume() {
@@ -25,14 +23,3 @@ window.onload = function() {
 }
 
 
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
-editor.session.setMode("ace/mode/json");
-editor.setValue(resumeObj);
-editor.removeSelectionMarker();
-
-editor.session.on('change', function(delta) {
-    var value = editor.getValue(); // or session.getValue
-    var resume = document.getElementById('resume');
-    resume.setAttribute('resume', JSON.stringify(JSON.parse(value)));
-});
