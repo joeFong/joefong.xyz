58371437cdaef7dec684d3947519abc1dc37b4d0
joe fong <joefong@joes-Air.hitronhub.home>
2019-11-01 18:28:24 -0700

    update

--- wp-content/themes/joefong/page-home.php
+++ wp-content/themes/joefong/page-home.php
@@ -8,6 +8,7 @@ get_header();
         <div id="editor"></div>
         <p class="code-text-container">Need a resume? But it takes too long to create one? Make one in 2 minutes with resume-jf, a lighting fast LitElement Web Component. Embeddable in any browser, and can be converted into a PDF in a click of a button.</p>
         <p class="code-link-container">
+            <button id="saveResume" title="saveMyResume">Save Resume</button>
             <a class="code-links" target="_blank" href="https://github.com/joeFong/resume-jf"><i class="fab fa-github"></i></a>
             <a class="code-links" target="_blank" href="https://www.npmjs.com/package/resume-jf"><i class="fab fa-npm"></i></a>
         </p>
@@ -16,6 +17,7 @@ get_header();
     <div id="github-mobile">
         <p class="code-text-container">Need a resume? But it takes too long to create one? Make one in 2 minutes with resume-jf, a lighting fast LitElement Web Component. Embeddable in any browser, and can be converted into a PDF in a click of a button.</p>
         <p class="code-link-container">
+            <button id="saveResume" title="saveMyResume">Save Resume</button>
             <a class="code-links" target="_blank" href="https://github.com/joeFong/resume-jf"><i class="fab fa-github"></i></a>
             <a class="code-links" target="_blank" href="https://www.npmjs.com/package/resume-jf"><i class="fab fa-npm"></i></a>
         </p>
